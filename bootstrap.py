2c6c68b78c903d87a175c8601ea2fbda1d3f905a
Rizul Goyal <goyalrizul1997@gmail.com>
2019-03-03 16:31:00 +0530

    Use of Jinja 2 template engine & Bootstrap CSS

--- bootstrap.py
+++ bootstrap.py
@@ -1,11 +1,17 @@
 from flask import Flask , render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route("/home")
 def index():
     return render_template("/home.html")
+
+@app.route("/css")
+def css():
+    return render_template("/css.html")
  
 
 @app.route("/about")
@@ -14,10 +20,9 @@ def about():
 
 @app.route("/contact")
 def contact():
-    fruits = ["Apple","Mango","Orange"]
-    return render_template("/contact.html",fruits = fruits)
+    return render_template("/contact.html")
 
 
 
 if __name__ == "__main__":
-    app.run(debug = True,port = 5001)  
+    app.run(debug = True,port = 5002)  
--- sql.py
+++ sql.py
@@ -7,23 +7,30 @@ app = Flask(__name__)
 
 # CONFIGURE DB
 
-db = yaml.load (open("db.yaml"))
+db = yaml.load(open("db.yaml"))
 app.config["MYSQL_HOST"] = db["mysql_host"]
 app.config["MYSQL_USER"] = db["mysql_user"]
 app.config["MYSQL_PASSWORD"] = db["mysql_password"]
 app.config["MYSQL_DB"] = db["mysql_db"]
 
-mysql = MySQL (app)
+mysql = MySQL(app)
  
 @app.route("/home")
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO user (user_name) VALUES (%s),(%s)", ["jony","shina"])
-    mysql.connection.commit()
+    # cur.execute("INSERT INTO user (user_name) VALUES (%s),(%s)", ["jony","shina"])
+    # mysql.connection.commit()
+    #To fetch values from database
 
+    result_value = cur.execute("SELECT * FROM user")
+    if result_value > 0:
+        users = cur.fetchall()
 
+        print(users[0])
+        # return str(users[0])
+        return users[0][0]
     return render_template("/home.html")
- 
+
 
 @app.route("/about")
 def about():
